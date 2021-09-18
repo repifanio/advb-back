1bba8c78cbdd7b7b1ec74f27faeb84b4220f4838
epifanio <rafael.epifanio@somosdx.co>
2021-09-18 15:16:17 -0300

    feat: documentation with swagger ok

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerConf from '@shared/config/swagger.json';
 import routes from '@/shared/http/routes';
 
 const app = express();
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConf));
 app.use(routes);
 
 export default app;
