137046fb1b7c333d571d4a25fe0f6c9c332ca4cd
Rafael Epifanio <rafael.epifanio@somosdx.co>
2022-04-08 12:57:03 -0300

    fix

--- src/modules/company/routes/company.router.ts
+++ src/modules/company/routes/company.router.ts
@@ -27,8 +27,6 @@ routes.get('/companyIndication/:idSetor', async (req, res) => {
   try {
     const { idSetor } = req.params;
 
-    console.log(req);
-
     const searchAllCompaniesToIndicate = new SearchAllCompaniesToIndicate();
     const companies = await searchAllCompaniesToIndicate.run(parseInt(idSetor, 10), req.user.userId);
 
