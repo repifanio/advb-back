49b517d11e07611869d2c05d8d5d301f135d68b2
Rafael Epifanio <rafael.epifanio@somosdx.co>
2022-04-25 13:09:03 -0300

    fix: alter company to vote

--- src/modules/votations/services/SearchAllIndicatesBySector.service.ts
+++ src/modules/votations/services/SearchAllIndicatesBySector.service.ts
@@ -4,7 +4,7 @@ import { iIndicates } from '@modules/votations/provider/indicates.provider';
 class SearchAllIndicatesBySector {
   public async run(sectorId: number):Promise<iIndicates[]> {
     const query: iIndicates[] = await getManager().query(`
-    SELECT i.indicate_id AS indicateId, i.description, c.company_id AS companyId, c.name FROM indicates i, companies c WHERE i.company_id = c.company_id and i.open_to_vote = true and i.sector_id = ? and c.accepted_to_vote = true;
+    SELECT i.indicate_id AS indicateId, i.description, c.company_id AS companyId, c.name FROM indicates i, companies c WHERE i.company_id = c.company_id and i.open_to_vote = true and i.sector_id = ? and c.accepted_to_vote = false;
   `, [sectorId]);
 
     return query;
